8b7609468c7658b1676dba9b27a38e56ab980703
mirankhan008 <mirankhan008@gmail.com>
2020-03-24 00:11:30 +0600

    first commit

---
+++ Character_length.py
@@ -0,0 +1,11 @@
+print("Enter The Charecter :")
+
+var1 = input()
+add = str(var1)
+
+def remove(add):
+    return add.replace(" ", "")
+
+print("Enter The Length Size:",len(remove(add)))
+print("Letter:",add)
+
